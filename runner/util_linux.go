85bfd0fa77c668d324277f8d427671fa2688e457
Eugene Denisenko <caravus@craft.sc>
2020-02-09 23:04:38 +0300

    SIGINT

--- runner/util_linux.go
+++ runner/util_linux.go
@@ -4,19 +4,30 @@ import (
 	"io"
 	"os/exec"
 	"syscall"
+	"time"
 
 	"github.com/creack/pty"
 )
 
-func killCmd(cmd *exec.Cmd) (int, error) {
-	pid := cmd.Process.Pid
+func (e *Engine) killCmd(cmd *exec.Cmd) (pid int, err error) {
+	pid = cmd.Process.Pid
+
+	if e.config.Build.SendInterrupt {
+		// Sending a signal to make it clear to the process that it is time to turn off
+		if err = syscall.Kill(-pid, syscall.SIGINT); err != nil {
+			return
+		}
+
+		time.Sleep(e.config.Build.KillDelay * time.Second)
+	}
+
 	// https://stackoverflow.com/questions/22470193/why-wont-go-kill-a-child-process-correctly
-	err := syscall.Kill(-pid, syscall.SIGKILL)
+	err = syscall.Kill(-pid, syscall.SIGKILL)
 
 	// Wait releases any resources associated with the Process.
 	_, _ = cmd.Process.Wait()
 
-	return pid, err
+	return
 }
 
 func (e *Engine) startCmd(cmd string) (*exec.Cmd, io.ReadCloser, io.ReadCloser, error) {
