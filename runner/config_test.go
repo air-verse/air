8ffde36474a6e48c0bb77ebf70ddb64f502ad3fa
施国鹏 <1033404553@qq.com>
2020-06-23 00:42:18 +0800

    Fixed configuration using Unix under Windows (#74)
    * Fixed configuration using Unix under Windows
    * add config test
    
    Co-authored-by: seth-shi <shi.guopeng@csdigit.com>

---
+++ runner/config_test.go
@@ -0,0 +1,65 @@
+package runner
+
+import (
+	"runtime"
+	"strings"
+	"testing"
+)
+
+const (
+	bin = `./tmp/main`
+	cmd = "go build -o ./tmp/main ."
+)
+
+func getWindowsConfig() config {
+	build := cfgBuild{
+		Cmd:         "go build -o ./tmp/main .",
+		Bin:         "./tmp/main",
+		Log:         "build-errors.log",
+		IncludeExt:  []string{"go", "tpl", "tmpl", "html"},
+		ExcludeDir:  []string{"assets", "tmp", "vendor"},
+		Delay:       1000,
+		StopOnError: true,
+	}
+	if runtime.GOOS == "windows" {
+		build.Bin = bin
+		build.Cmd = cmd
+	}
+
+	return config{
+		Root:   ".",
+		TmpDir: "tmp",
+		Build:  build,
+	}
+}
+
+func TestBinCmdPath(t *testing.T) {
+
+	var err error
+
+	c := getWindowsConfig()
+	err = c.preprocess()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if runtime.GOOS == "windows" {
+
+		if ! strings.HasSuffix(c.Build.Bin, "exe") {
+			t.Fail()
+		}
+
+		if ! strings.Contains(c.Build.Bin, "exe") {
+			t.Fail()
+		}
+	} else {
+
+		if strings.HasSuffix(c.Build.Bin, "exe") {
+			t.Fail()
+		}
+
+		if strings.Contains(c.Build.Bin, "exe") {
+			t.Fail()
+		}
+	}
+}
