9557343eea87c7b5d2d66f1ffb174eb27a5edec6
Dennis Schön <mail@dennis-schoen.de>
2022-02-25 11:38:11 +0100

    :bug: Fix interrupt handling on darwin (#257)
    *:bug: fix interrupt handling on darwin
    
    *:bug: send interrupt to process group pid
    
    Co-authored-by: Dennis Schön <dennis.schoen@aroundhome.de>

--- runner/util_darwin.go
+++ runner/util_darwin.go
@@ -12,18 +12,18 @@ import (
 
 func (e *Engine) killCmd(cmd *exec.Cmd) (pid int, err error) {
 	pid = cmd.Process.Pid
+	pgid, err := syscall.Getpgid(cmd.Process.Pid)
+	if err != nil {
+		return pgid, errors.Wrapf(err, "failed to get pgid, pid %v", pid)
+	}
 
 	if e.config.Build.SendInterrupt {
 		// Sending a signal to make it clear to the process that it is time to turn off
-		if err = syscall.Kill(pid, syscall.SIGINT); err != nil {
+		if err = syscall.Kill(-pgid, syscall.SIGINT); err != nil {
 			e.mainDebug("trying to send signal failed %v", err)
 			return
 		}
 		time.Sleep(e.config.Build.KillDelay * time.Millisecond)
-	}
-	pgid, err := syscall.Getpgid(cmd.Process.Pid)
-	if err != nil {
-		return pgid, errors.Wrapf(err, "failed to get pgid, pid %v", pid)
 	}
 	err = syscall.Kill(-pgid, syscall.SIGKILL)
 	if err != nil {
