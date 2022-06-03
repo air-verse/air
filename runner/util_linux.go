60b3db93428b191bdb8459b3d30a7d32cee3b286
xiantang <zhujingdi1998@gmail.com>
2022-06-03 23:18:29 +0800

    add test for dlv port already used

--- runner/util_linux.go
+++ runner/util_linux.go
@@ -10,26 +10,21 @@ import (
 )
 
 func (e *Engine) killCmd(cmd *exec.Cmd) (pid int, err error) {
-	// https://groups.google.com/g/golang-nuts/c/XoQ3RhFBJl8
-	// only use (p *Process) Kill() will just kill the process, but it won't also the child process in linux
-	pgid, err := syscall.Getpgid(cmd.Process.Pid)
-	if err != nil {
-		return pgid, err
-	}
+	pid = cmd.Process.Pid
 
 	if e.config.Build.SendInterrupt {
 		// Sending a signal to make it clear to the process that it is time to turn off
-		if err = syscall.Kill(-pgid, syscall.SIGINT); err != nil {
+		if err = syscall.Kill(-pid, syscall.SIGINT); err != nil {
 			return
 		}
 		time.Sleep(e.config.Build.KillDelay * time.Millisecond)
 	}
 
-	e.mainDebug("got pgid %v", pgid)
-	if err = syscall.Kill(-pgid, syscall.SIGKILL); err != nil {
-		return pgid, err
-	}
-	e.mainDebug("killed process pid %d successed", pgid)
+	// https://stackoverflow.com/questions/22470193/why-wont-go-kill-a-child-process-correctly
+	err = syscall.Kill(-pid, syscall.SIGKILL)
+
+	// Wait releases any resources associated with the Process.
+	_, _ = cmd.Process.Wait()
 	return
 }
 
