47cd3166c3f9151d939d31a646c67b5074876378
xiantang <zhujingdi1998@gmail.com>
2022-07-02 12:34:37 +0800

    Add unset value

--- runner/flag.go
+++ runner/flag.go
@@ -4,12 +4,14 @@ import (
 	"flag"
 )
 
+const UnsetDefault = "DEFAULT"
+
 // ParseConfigFlag parse toml information for flag
 func ParseConfigFlag(f *flag.FlagSet) map[string]TomlInfo {
 	c := Config{}
 	m := flatConfig(c)
 	for k, v := range m {
-		f.StringVar(v.Value, k, "", "")
+		f.StringVar(v.Value, k, UnsetDefault, "")
 	}
 	return m
 }
