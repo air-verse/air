b478e5594f2ebb95e77be209730c64f998f55681
Kareem Ebrahim <kareemmahlees@gmail.com>
2024-05-26 19:44:57 +0300

    fix: transform build.bin path to absolute path

--- runner/config.go
+++ runner/config.go
@@ -384,8 +384,13 @@ func (c *Config) rel(path string) string {
 
 // WithArgs returns a new config with the given arguments added to the configuration.
 func (c *Config) WithArgs(args map[string]TomlInfo) {
-	for _, value := range args {
+	for key, value := range args {
 		if value.Value != nil && *value.Value != unsetDefault {
+			// To avoid path resolving problems on Windows.
+			if key == "build.bin" {
+				abs, _ := filepath.Abs(*value.Value)
+				value.Value = &abs
+			}
 			v := reflect.ValueOf(c)
 			setValue2Struct(v, value.fieldPath, *value.Value)
 		}
