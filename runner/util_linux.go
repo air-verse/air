4d26204c631442e777b7f0156feba509a332880c
Isak Styf <isak.styf@masarinconsulting.se>
2024-11-01 16:39:27 +0100

    abort kill delay if process closes after interrupt
    if the process to be killed honours the SIGINT and closes down,
    we should not sleep before trying to kill a process that is
    no longer there

--- runner/util_linux.go
+++ runner/util_linux.go
@@ -1,6 +1,7 @@
 package runner
 
 import (
+	"context"
 	"io"
 	"os/exec"
 	"syscall"
@@ -12,20 +13,50 @@ import (
 func (e *Engine) killCmd(cmd *exec.Cmd) (pid int, err error) {
 	pid = cmd.Process.Pid
 
+	var killDelay time.Duration
+
 	if e.config.Build.SendInterrupt {
+		e.mainDebug("sending interrupt to process %d", pid)
 		// Sending a signal to make it clear to the process that it is time to turn off
 		if err = syscall.Kill(-pid, syscall.SIGINT); err != nil {
 			return
 		}
-		time.Sleep(e.config.killDelay())
+		// the kill delay is 0 by default unless the user has configured send_interrupt=true
+		// in which case it is fetched from the kill_delay setting in the .air.toml
+		killDelay = e.config.killDelay()
+		e.mainDebug("setting a kill timer for %s", killDelay.String())
 	}
 
-	// https://stackoverflow.com/questions/22470193/why-wont-go-kill-a-child-process-correctly
-	err = syscall.Kill(-pid, syscall.SIGKILL)
+	waitResult := make(chan error)
+	go func() {
+		defer close(waitResult)
+		_, _ = cmd.Process.Wait()
+	}()
 
-	// Wait releases any resources associated with the Process.
-	_, _ = cmd.Process.Wait()
-	return
+	// prepare a cancel context that can stop the killing if it is not needed
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	killResult := make(chan error)
+	// Spawn a goroutine that will kill the process after kill delay if we have not
+	// received a wait result before that.
+	go func() {
+		select {
+		case <-time.After(killDelay):
+			// https://stackoverflow.com/questions/22470193/why-wont-go-kill-a-child-process-correctly
+			killResult <- syscall.Kill(-pid, syscall.SIGKILL)
+		case <-ctx.Done():
+			return
+		}
+	}()
+
+	for {
+		select {
+		case err = <-killResult:
+		case <-waitResult:
+			return
+		}
+	}
 }
 
 func (e *Engine) startCmd(cmd string) (*exec.Cmd, io.ReadCloser, io.ReadCloser, error) {
